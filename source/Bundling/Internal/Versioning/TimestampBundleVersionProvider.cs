e4aa23294a611b19db1e9a8abc2f3d2932ef5816
Adam Simon <adamosimoni@gmail.com>
2020-09-03 11:54:55 +0200

    minor optimization

--- source/Bundling/Internal/Versioning/TimestampBundleVersionProvider.cs
+++ source/Bundling/Internal/Versioning/TimestampBundleVersionProvider.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.WebUtilities;
+﻿using System;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace Karambolo.AspNetCore.Bundling.Internal.Versioning
 {
@@ -8,7 +9,12 @@ namespace Karambolo.AspNetCore.Bundling.Internal.Versioning
         {
             var ticks = context.Timestamp.Ticks;
 
+#if NETSTANDARD2_0
             var bytes = new byte[sizeof(long)];
+#else
+            Span<byte> bytes = stackalloc byte[sizeof(long)];
+#endif
+
             for (var i = 0; i < sizeof(long); i++)
                 bytes[i] = (byte)(ticks >> ((sizeof(long) - 1 - i) << 3) & 0xFF);
 
